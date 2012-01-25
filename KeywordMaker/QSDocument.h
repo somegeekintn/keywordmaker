182206003f123705498d850d7faa4b0ce1fbbdaf
Casey Fleser <cfleser@gmail.com>
2012-01-24 19:37:59 -0600

    Initial Commit

---
+++ KeywordMaker/QSDocument.h
@@ -0,0 +1,13 @@
+//
+//  QSDocument.h
+//  KeywordMaker
+//
+//  Created by Casey Fleser on 1/24/12.
+//  Copyright (c) 2012 Griffin Technology, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface QSDocument : NSDocument
+
+@end
---
+++ KeywordMaker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KeywordMaker
+//
+//  Created by Casey Fleser on 1/24/12.
+//  Copyright (c) 2012 Griffin Technology, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
